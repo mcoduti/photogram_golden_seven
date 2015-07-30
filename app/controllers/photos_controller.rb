8d2c8423e6c3d8064e30e4112365e3b8c345a27f
mcoduti <matthew.coduti@gmail.com>
2015-07-29 20:54:45 -0500

    up to edit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,33 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_id = params["id"]
+    @list_of_photos = Photo.all
+    @caption = @list_of_photos.find(@photo_id).caption
+    @photo = @list_of_photos.find(@photo_id).source
+  end
 
+  def new_form
+  end
+
+  def destroy
+
+    @photo_id = params["id"]
+    @list_of_photos = Photo.all
+    @list_of_photos.find(@photo_id).destroy
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def create_photo
+    # raise params.inspect
+    @input_caption = params["the_caption"]
+    @input_source = params["the_source"]
+
+    i = Photo.new
+    i.caption = @input_caption
+    i.source = @input_source
+    i.save
+  end
 end
